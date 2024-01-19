dd17ff31989d5e043a19961f3aed4e8698ed9ce8
Mateus Schverz <matefs8569@gmail.com>
2024-01-19 19:56:29 -0300

    add: added a new datetime route to just show  initial and final date.

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
+from datetime import datetime
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class DateParams(BaseModel):
+    initial_date: datetime
+    final_date: datetime
 
-@app.get("/hello/{name}")
-def read_item(name: str):
-    return {"Hello": name}
+#Todo: calculate the difference between the two dates
 
+@app.get("/datetime/difference")
+async def return_date_difference(
+    initial_date: datetime,
+    final_date: datetime,
+    format: Optional[str] = "json"  # Adicionando um parâmetro opcional para especificar o formato
+):
+    if format == "json":
+        return {"initial_date": initial_date, "final_date": final_date}
+    elif format == "text":
+        return f"Inicial: {initial_date}, Final: {final_date}"
+    else:
+        return {"error": "Formato inválido, use 'json' ou 'text'"}
