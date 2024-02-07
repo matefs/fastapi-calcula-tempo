02550269763bd09735d61ed93871233d90fec227
Mateus Schverz <matefs8569@gmail.com>
2024-02-07 15:58:12 -0300

    feat:  it will validate if final date is after initial date

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ async def return_date_difference(
         final_date: datetime,
         format: Optional[str] = "json"  # Adicionando um parâmetro opcional para especificar o formato
 ):
+    if (initial_date > final_date):
+        return {"error": "Data inicial não pode ser maior do que data final"}
     if (validate_JSON_format(format)):
         date_difference: datetime = final_date - initial_date
         time_difference_in_seconds: float = date_difference.total_seconds()
         return {"initial_date": initial_date, "final_date": final_date, "date_difference": str(date_difference), "seconds": time_difference_in_seconds}
-
+    
     return {"error": "Formato inválido, use 'json' ou 'text'"}
 
 @app.get('/datetime/sum')
@@ -30,6 +32,7 @@ async def return_date_sum(initial_date: datetime, final_date: datetime, format: 
         return {"initial_date": initial_date, "final_date": final_date, "date_sum": str(date_sum) }
     return {"error": "Formato inválido, use 'json' ou 'text'"}
 
-@app.get('/',include_in_schema=False)
+
+@app.get('/', include_in_schema=False)
 async def main():
     return RedirectResponse(url="/docs")
