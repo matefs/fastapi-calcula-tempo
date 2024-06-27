33dacdcdd3a1d128652400b4b822c31166cb97ad
Mateus Schverz <matefs8569@gmail.com>
2024-06-27 18:40:03 -0300

    wip: return years, days, months, hours, seconds

--- main.py
+++ main.py
@@ -11,28 +11,27 @@ app = FastAPI()
 
 
 @app.get("/datetime/difference")
-async def return_date_difference(
+async def return_date_time_difference(
         initial_date: datetime,
         final_date: datetime,
         format: Optional[str] = "json"  # Adicionando um parâmetro opcional para especificar o formato
 ):
-    if (initial_date > final_date):
-        return {"error": "Data inicial não pode ser maior do que data final"}
-    if (validate_JSON_format(format)):
-        date_difference: datetime = final_date - initial_date
-        time_difference_in_seconds: float = date_difference.total_seconds()
-        return {"initial_date": initial_date, "final_date": final_date, "date_difference": str(date_difference), "seconds": time_difference_in_seconds}
-    
-    return {"error": "Formato inválido, use 'json' ou 'text'"}
+    date_time_difference:datetime = None 
 
-@app.get('/datetime/sum')
-async def return_date_sum(initial_date: datetime, final_date: datetime, format: Optional[str] = "json"):
+    if initial_date > final_date: return {"error": "Data inicial não pode ser maior do que data final"}
+
     if (validate_JSON_format(format)):
-        date_sum: timedelta = final_date + initial_date
-        return {"initial_date": initial_date, "final_date": final_date, "date_sum": str(date_sum) }
+        datetime_difference = final_date - initial_date
+        # time_difference_in_days = datetime_difference.days
+        # time_difference_in_hours = datetime_difference.hours
+        # time_difference_in_months = datetime_difference.months
+        time_difference_in_seconds: float = date_time_difference.total_seconds()
+        return {"initial_date": initial_date, "final_date": final_date, "date_time_difference": str(date_time_difference), "seconds": time_difference_in_seconds}
+    
     return {"error": "Formato inválido, use 'json' ou 'text'"}
 
 
 @app.get('/', include_in_schema=False)
 async def main():
     return RedirectResponse(url="/docs")
+
